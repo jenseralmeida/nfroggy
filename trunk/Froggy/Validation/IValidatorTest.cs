6b2c6a1153c8f8feb94039d9d4518e9fbca53a6a
jenseralmeida <jenseralmeida@76df76a6-6046-0410-ae9b-e9df90dfb79b>
2008-03-05 01:27:19 +0000

    First version with a good architecture

--- trunk/Froggy/Validation/IValidatorTest.cs
+++ trunk/Froggy/Validation/IValidatorTest.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Froggy.Validation
 {
-    public interface IValidatorTest<T>: IValidator<T>
+    public interface IValidatorTest<T>: IValidator
     {
         bool Execute(T value);
     }
