094cc7b926eb813f9af78e248e4de6c0317968e1
jenseralmeida <jenseralmeida@76df76a6-6046-0410-ae9b-e9df90dfb79b>
2008-07-12 16:59:05 +0000

    add start of the validator - object integration

---
+++ Froggy.Test/Validation/Object/Stub.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Froggy.Validation.Object;
+using Froggy.Validation;
+
+namespace Froggy.Test.Validation.Object
+{
+    class Stub
+    {
+        string name;
+
+        [Validator(false, new StupNameTestValidator())]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        class StupNameTestValidator : ITestValidator<string>
+        {
+            #region ITestValidator<string> Members
+
+            public bool Execute(string value, out string errorMessageTemplate)
+            {
+                switch (value)
+                {
+                    case "a":
+                    case "e":
+                    case "i":
+                    case "o":
+                    case "u":
+                        errorMessageTemplate = "";
+                        break;
+                    default:
+                        errorMessageTemplate = "o valor para {0} não é uma vogal";
+                }
+            }
+
+            #endregion
+        }
+    }
+}
