5b8e6a7d47577b68c873296169fe667ee8d1ea71
viniciusasdf <viniciusasdf@76df76a6-6046-0410-ae9b-e9df90dfb79b>
2008-07-23 16:06:41 +0000

    pequena modificação na classe Foo (só pra registrar uma primeira contribuição

--- trunk/Froggy.Data.Db4o.Test/Foo.cs
+++ trunk/Froggy.Data.Db4o.Test/Foo.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using Froggy.Validation.Object;
 using Froggy.Validation.BaseValidator;
 
@@ -9,21 +6,10 @@ namespace Froggy.Data.Db4o.Test
 {
     public class Foo
     {
-        string _Name;
-        int _Age;
-
-        [SystemTypeValidator(IsNullable=false)]
-        public string Name
-        {
-            get { return _Name; }
-            set { _Name = value; }
-        }
+        [SystemTypeValidator(IsNullable = false)]
+        public string Name { get; set; }
 
         [ComparableValidator(0, 200, IntervalValidatorType.IntervalInclusive)]
-        public int Age
-        {
-            get { return _Age; }
-            set { _Age = value; }
-        }
+        public int Age { get; set; }
     }
 }
